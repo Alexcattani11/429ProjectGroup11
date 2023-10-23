4e7ce151a36833338b37d75221ec416d17c8e472
YongDeYu <yongde.yu@mail.mcgill.ca>
2023-10-22 22:43:02 -0400

    finish updating tests for projectsidtasks

--- test/jsontests/projectsidtasks.test.js
+++ test/jsontests/projectsidtasks.test.js
@@ -64,27 +64,30 @@ afterEach(async() => {
     await request(constants.HOST).delete(`/todos/${ourTodo.id}`).send();
 });
 
-describe("/todos/:id/tasksof", () => {
+describe("/projects/:id/tasks", () => {
     describe("GET", () => {
 
-        it("returns tasks for related project and todo", async() => {
+        it("return all the todo items related to project, with given id, by the relationship named tasks", async() => {
              
-            const postResponse = await request(constants.HOST).post(`/todos/${ourTodo.id}/tasksof`).send({
+            const postResponse = await request(constants.HOST).post(`/projects/${ourProject.id}/tasks`).send({
                 id: ourProject.id
             });
             expect(postResponse.statusCode).toEqual(201);
 
-            const response = await request(constants.HOST).get(`/todos/${ourTodo.id}/tasksof`).send();
+            const response = await request(constants.HOST).get(`/projects/${ourProject.id}/tasks`).send();
             expect(response.statusCode).toEqual(200);
-            expect(response.body.projects[0].id).toEqual(ourProject.id);
-            expect(response.body.projects[0].title).toEqual(ourProject.title);
-            expect(response.body.projects[0].completed).toEqual(ourProject.completed.toString());
-            expect(response.body.projects[0].active).toEqual(ourProject.active.toString());
-            expect(response.body.projects[0].description).toEqual(ourProject.description);
+            expect(response.body.projects[0].id).toEqual(ourTodo[0].id);
+            expect(response.body.projects[0].title).toEqual(ourTodo[0].title);
+            expect(response.body.projects[0].completed).toEqual(ourTodo[0].doneStats.toString());
+            expect(response.body.projects[0].description).toEqual(ourTodo[0].description);
+			expect(response.body.projects[1].id).toEqual(ourTodo[1].id);
+            expect(response.body.projects[1].title).toEqual(ourTodo[1].title);
+            expect(response.body.projects[1].completed).toEqual(ourTodo[1].doneStats.toString());
+            expect(response.body.projects[1].description).toEqual(ourTodo[1].description);
         });
 
         it("returns no tasks for unrelated project and todo", async() => {
-            const response = await request(constants.HOST).get(`/todos/${ourTodo.id}/tasksof`).send();
+            const response = await request(constants.HOST).get(`/projects/${ourProject.id}/tasks`).send();
             expect(response.statusCode).toEqual(200);
             expect(response.body.projects.length).toEqual(0);
         });
@@ -93,7 +96,7 @@ describe("/todos/:id/tasksof", () => {
 
     describe("HEAD", () => {
         it("returns JSON as default", async() => {
-            const response = await request(constants.HOST).head(`/todos/${ourTodo.id}/tasksof`).send();
+            const response = await request(constants.HOST).head(`/projects/${ourProject.id}/tasks`).send();
 
             expect(response.statusCode).toEqual(200);
             expect(response.headers["content-type"]).toEqual("application/json");
@@ -102,9 +105,9 @@ describe("/todos/:id/tasksof", () => {
 
     describe("POST", () => {
 
-        it("should create a taskof relation with a project and todo with valid id", async() => {
+        it("create an instance of a relationship named tasks between project instance :id and the todo instance represented by the id in the body of the message", async() => {
 
-            const postResponse = await request(constants.HOST).post(`/todos/${ourTodo.id}/tasksof`).send({
+            const postResponse = await request(constants.HOST).post(`/projects/${ourProject.id}/tasks`).send({
                 id: ourProject.id
             });
 
@@ -113,21 +116,26 @@ describe("/todos/:id/tasksof", () => {
             expect(postResponse.body).toEqual("");
 
             // use get to check if it's there
-            const getResponse = await request(constants.HOST).get(`/todos/${ourTodo.id}/tasksof`).send();
+            const getResponse = await request(constants.HOST).get(`/projects/${ourProject.id}/tasks`).send();
             expect(getResponse.statusCode).toEqual(200);
-            expect(getResponse.body.projects[0].id).toEqual(ourProject.id);
-            expect(getResponse.body.projects[0].title).toEqual(ourProject.title);
-            expect(getResponse.body.projects[0].completed).toEqual(ourProject.completed.toString());
-            expect(getResponse.body.projects[0].active).toEqual(ourProject.active.toString());
-            expect(getResponse.body.projects[0].description).toEqual(ourProject.description);
+			expect(response.statusCode).toEqual(200);
+            expect(response.body.projects[0].id).toEqual(ourTodo[0].id);
+            expect(response.body.projects[0].title).toEqual(ourTodo[0].title);
+            expect(response.body.projects[0].completed).toEqual(ourTodo[0].doneStats.toString());
+            expect(response.body.projects[0].description).toEqual(ourTodo[0].description);
+			expect(response.body.projects[1].id).toEqual(ourTodo[1].id);
+            expect(response.body.projects[1].title).toEqual(ourTodo[1].title);
+            expect(response.body.projects[1].completed).toEqual(ourTodo[1].doneStats.toString());
+            expect(response.body.projects[1].description).toEqual(ourTodo[1].description);
+			
         });
 
-        it("should not create taskof relation with invalid project id", async() => {
+        it("should not create tasks relation with invalid project id", async() => {
             const invalidId = -1;
 
             const expectedError = "Could not find thing matching value for id";
 
-            const response = await request(constants.HOST).post(`/todos/${ourTodo.id}/tasksof`).send({
+            const response = await request(constants.HOST).post(`/projects/${ourProject.id}/tasks`).send({
                 id: invalidId
             });
 
@@ -135,12 +143,12 @@ describe("/todos/:id/tasksof", () => {
             expect(response.body.errorMessages[0]).toEqual(expectedError);
         });
 
-        it("should not create taskof relation with invalid todo id", async() => {
+        it("should not create tasks relation with invalid project id", async() => {
             const invalidId = -1;
 
-            const expectedError = "Could not find parent thing for relationship todos/-1/tasksof";
+            const expectedError = "Could not find parent thing for relationship projects/-1/tasks";
 
-            const response = await request(constants.HOST).post(`/todos/${invalidId}/tasksof`).send({
+            const response = await request(constants.HOST).post(`/projects/${invalidId}/tasks`).send({
                 id: ourProject.id
             });
 
